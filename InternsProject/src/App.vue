df03ba7d2bdaf32f0649749ec33e06705e17f379
sianmae <t.canton@cqumail.com>
2023-07-26 15:29:04 +1000

    created button and logo

--- InternsProject/src/App.vue
+++ InternsProject/src/App.vue
@@ -1,24 +1,16 @@
 <script setup lang="ts">
+import Logo from './components/Logo.vue'
+import ButtonTemplate from './components/Button.vue'
 </script>
 
 <template>
   <v-layout class="rounded rounded-md">
-    <v-app-bar color="surface-variant" title="Interns Project"></v-app-bar>
-
-    <v-navigation-drawer>
-      <v-list>
-        <v-list-item title="Drawer left"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-navigation-drawer location="right">
-      <v-list>
-        <v-list-item title="Drawer right"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <v-app-bar color="surface-variant" height="90">
+      <Logo />
+    </v-app-bar>
 
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-      Main Content
+      <ButtonTemplate />
     </v-main>
   </v-layout>
 </template>
---
+++ InternsProject/src/components/Logo.vue
@@ -0,0 +1,6 @@
+<template>
+    <v-toolbar-title class="d-flex py-3">
+        <v-img src="./src/assets/images/pokemon_logo.png" max-height="36" contain class="mt-3"/>
+        Pokemon War
+    </v-toolbar-title>
+</template>
