679dc283c9d58c28e5227682b328af20db8f22c1
R-Sudharsan <sudhrravi@gmail.com>
2023-07-26 16:29:24 +1000

    axios and model added

---
+++ InternsProject/src/model/Pokemon.ts
@@ -0,0 +1,29 @@
+export interface Pokemon {
+    num: number;
+    name: string;
+    variations: Variation[];
+    link: string;
+}
+
+export interface Variation {
+    name: string;
+    description: string;
+    image: string;
+    types: string[];
+    specie: string;
+    height: number;
+    weight: number;
+    abilities: string[];
+    stats: Stats;
+    evolutions: string[];
+}
+
+export interface Stats {
+    total: number;
+    hp: number;
+    attack: number;
+    defense: number;
+    speedAttack: number;
+    speedDefense: number;
+    speed: number;
+}
---
+++ InternsProject/src/services/axios.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+// import { Pokemon } from "../model/Pokemon"
+async function getAllPokemon(): Promise<any[]> {
+    try {
+        const response = await axios.get<any[]>('http://localhost:3000/pokedex');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching Pokémon data:', "error message here");
+        throw error;
+    }
+}
