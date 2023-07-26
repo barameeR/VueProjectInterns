8527f242300441944380c8ba68d9e1c6c3ee1b15
panghulan-cqu <j.panghulan@cqumail.com>
2023-07-26 15:43:28 +1000

    Adjustment of Stage

--- InternsProject/src/components/main.vue
+++ InternsProject/src/components/main.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
     import { ref } from 'vue'
-    const imageClass = 'image'
+    const imageClass = 'image-container'
 </script>
 
 <template>
-    <img :class='imageClass' src="../assets/design/stage.png"/>
+    
+    <div class="image-container">
+        <img :class='imageClass' src="../assets/design/stage.png"/>
+</div>
 </template>
 <style>
-.image{
-    width: 75%;
+.image-container {
+  max-width: 100%;
+  max-height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Set the container height to the full viewport height */
+}
+.image-container img {
+  max-width: 100%;
+  max-height: 100%;
+  height: auto;
 }
 </style>
