13b5a33a104e8941df2c792694fdeb18c65886ba
mhooboat <mooboat366@gmail.com>
2023-07-26 13:43:57 +1000

    Modified home page

--- InternsProject/src/App.vue
+++ InternsProject/src/App.vue
@@ -1,47 +1,28 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <v-layout class="rounded rounded-md">
+    <v-app-bar color="surface-variant" title="Application bar"></v-app-bar>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Drawer left"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <v-navigation-drawer location="right">
+      <v-list>
+        <v-list-item title="Drawer right"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      Main Content
+    </v-main>
+  </v-layout>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
