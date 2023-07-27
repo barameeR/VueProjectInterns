c59c8d65f638007add7b9efcef6440116041a383
mhooboat <mooboat366@gmail.com>
2023-07-27 13:49:32 +1000

    Game Logic

--- InternsProject/src/pages/Game.vue
+++ InternsProject/src/pages/Game.vue
@@ -1,29 +1,48 @@
 <template>
-  <Suspense>
-    <card v-for="(pokemon, index) in playerArray" :key="index" :pokemon="pokemon" />
-  </Suspense>
-</template>
+    <v-container>
+      <v-row>
+        <v-col v-for="(pokemon, index) in playerArray" :key="index" cols="auto">
+          <Suspense>
+            <card :pokemon="pokemon" playerType ="player" :initialActive="isButtonActive" @button-clicked="onButtonClicked"/>
+          </Suspense>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col v-for="(pokemon, index) in playArea" :key="index" cols="auto">
+          <Suspense>
+            <card :pokemon="pokemon" playerType ="playArea" :initialActive="isButtonActive" @button-clicked="onButtonClicked"/>
+          </Suspense>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col v-for="(pokemon, index) in computerArray" :key="index" cols="auto">
+          <Suspense>
+            <card :pokemon="pokemon" playerType ="enemy" />
+          </Suspense>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
   
 <script setup lang="ts">
 import { defineAsyncComponent, ref, onMounted } from 'vue';
-import { getPokemonById } from '@/services/axios';
 import Card from '../components/card.vue'; // Correct the import path and component name if needed
-import { type Pokemon } from '@/model/Pokemon';
 import { getAllPokemon } from "../services/axios"
 
 const playerArray = ref([])
 const computerArray = ref([])
+const playArea = ref([])
+const isButtonActive = ref(false)
+const onButtonClicked = (isActive: boolean) => {
+  // Update the active state in the parent component based on the emitted value
+  isButtonActive.value = isActive;
+};
 
 var pokeData = getAllPokemon()
   .then((pokemonData) => {
     const [array1, array2] = chooseTwoArrays(pokemonData);
     playerArray.value = array1
     computerArray.value = array2
-    console.log("Enemy / Computer Array 1:");
-    console.log(array1);
-    console.log("Player Array 2:");
-    console.log(array2);
-
   })
   .catch((error) => {
     console.error('Error:', error);
@@ -61,25 +80,31 @@ function chooseTwoArrays(data) {
 
   return [partition1, partition2];
 }
-// // Define the prop
-// const pokemonData = ref<Pokemon[]>([]);
 
-// // Use a regular function to handle the asynchronous operation
-// const fetchData = async () => {
-//   try {
-//     const data = await getPokemonById(10);
-//     pokemonData.value = data;
-//     console.log(pokemonData.value)
-//   } catch (error) {
-//     console.error('Error fetching Pokémon data:', error);
-//   }
-// };
+function play (index: number , playerType: string){
+    if (playerType == "player"){
+        playArea.value.push(playerArray.value.splice(1,index)[0])
 
-// // Fetch the data when the component is mounted
-// onMounted(() => {
-//   fetchData();
-// });
+    }else{
+        console.log("test Plaey area enemy",)
+        playArea.value.push(computerArray.value[index])
+        // computerArray.value.splice(1,index)[0]
+        console.log(playArea.value)
+    }
+}
 
-// // Use defineAsyncComponent to define the Card component asynchronously
-// const card = defineAsyncComponent(() => import('../components/card.vue'));
+function enemyPlay (){
+    const randomIndex = Math.floor(Math.random() * computerArray.value.length);
+    console.log('AnyThing')
+    play(computerArray.value[randomIndex],'enemy') ;
+}
+
+
+onMounted(()=>{
+    enemyPlay()
+})
+
 </script>
+
+<style>
+</style>
