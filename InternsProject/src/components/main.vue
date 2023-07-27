251546b81e4b3d97585bae54e1e8b6957ca1a9f2
sianmae <t.canton@cqumail.com>
2023-07-27 15:12:44 +1000

    banner and button alignment

--- InternsProject/src/components/main.vue
+++ InternsProject/src/components/main.vue
@@ -4,25 +4,56 @@ const imageClass = ref('image-container')
 </script>
 
 <template>
-  <div class="center-div">
-    <div class="single-column-button">
-      <slot></slot>
+  <div class="container">
+    <div class="banner"></div>
+    <div class="center-div">
+      <div class="single-column-button">
+        <slot></slot>
+      </div>
     </div>
   </div>
   <!-- <img :class='imageClass' src="../assets/design/stage.png"/> -->
 </template>
 <style>
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;  
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50vw;
+  margin-top: 20px;
+  /* min-height: 100vh; This ensures the container takes up at least the full height of the viewport */
+}
+.banner {
+  width: 100%;
+  height: 300px;
+  /* Set the desired height of the div */
+  background-image: url('../assets/design/bg2.png');
+  background-size: contain;
+  /* Adjust to 'contain' if you want to keep the image's aspect ratio */
+  background-position: center center;
+}
+
 .single-column-button {
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
 
 .center-div {
-  display: flex;
+  /* display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  width: 100vw; */
   /* Optional: This sets the container to be the full height of the viewport */
+  /* margin-top: 20px; */
 }
 
 .image-container {
